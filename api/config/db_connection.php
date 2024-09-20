4cb54d21530c3ec53394d4fb96b9843805697e6e
juan <gamerjuancho88@gmail.com>
2024-09-20 12:03:36 -0500

    adios

--- api/config/db_connection.php
+++ api/config/db_connection.php
@@ -1,4 +1,6 @@
 <?php
+
+/*
 $number1 = 20;
 $number2 = 12;
 
@@ -6,6 +8,45 @@ $number2 = 12;
 $number3 = $number1 + $number2;
 
 echo $number3;
+
+*/
+
+
+
+//database conetion 
+//developer juan chaucanez  
+//data quemada
+$servername = "localhost";//127.0.0.1;localhost
+$username = "postgres";//credenciales 
+$password = "unicesmag";
+//nombre base datos
+$dbname = "beta";
+$port = "5432";
+
+//credencial de conectividad
+$data_connection = "
+host   = $host
+port   = $port
+dbname = $dbname
+user   = $username
+password = $password
+";
+//conectar al puerto
+$conn = pg_connect($data_connection);
+//da permisos 
+//solo para los programadores 
+if (!$conn) {
+    die("cagaste mano ". pg_last_error());
+} else {
+
+    echo "conectado mano";
+ 
+}
+
+
+
+pg_close($conn);
+
 
 ?>
 
