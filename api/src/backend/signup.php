3a34f04f64183778827961e45a560b6dbd473e88
juan <gamerjuancho88@gmail.com>
2024-10-10 12:22:52 -0500

    api

--- api/src/backend/signup.php
+++ api/src/backend/signup.php
@@ -1,10 +1,21 @@
 <?php 
+require "../../config/db_connection.php";
 
 $email = $_POST ['emailalias'];
-
 $pass = $_POST ['passwd'];
 
 $enc_pass = md5($pass);
+
+$query = "select * from users where email = '$email'";
+$result = pg_query($conn, $query);
+$row = pg_fetch_assoc($result);
+if($row) {
+    //echo "Registro exitoso!";
+    echo "<script>alert ('Email already exists!')</script>";
+    header ('refresh:0; url=http://127.0.0.1/BETA/api/src/register_form.html');
+    exit();
+}
+
 
 
 //DB.connection
@@ -13,13 +24,16 @@ require('../../config/db_connection.php');
  
 //GET data from register_form
 
-$query = "INSERT INTO users (email,password) VALUES('$email',' $enc_pass')";
+$query = "INSERT INTO users (email,password) VALUES('$email',' $enc_pass')"; //execute querry
+
 
 $result = pg_query($conn, $query);
 
 
 if($result) {
-    echo "Registro exitoso!";
+    //echo "Registro exitoso!";
+    echo "<script>alert ('registration successful!')</script>";
+    header ('refresh:0; url=http://127.0.0.1/BETA/api/src/login_form.html');
 } else {
     echo "Error en el registro!";
 }
@@ -33,6 +47,4 @@ pg_close($conn);
 /* CREATE TABLE users (id SERIAL PRIMARY KEY,email varchar(150), password TEXT NOT NULL, status BOOLEAN DEFAULT TRUE , created_at T
 IMESTAMPTZ DEFAULT NOW() , update_at TIMESTAMPTZ DEFAULT NOW(), deleted_at TIMESTAMPTZ);
 */
-
-
 ?>
